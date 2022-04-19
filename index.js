6af6716cac61b178343a3c483678411f4807b1e6
+ <+>
2022-04-19 13:52:16 -0400

    commit tree 0bd28417a0e22a82a0a0f8a79868165ec8d09b16

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  let checkTrue = '';
+  for(let i = word.length -1; i >= 0 ; i--) {
+    checkTrue = checkTrue + word[i]
+  }
+    return checkTrue === word; 
+  }
+    
+
+  /*
+  Alt solution with array as shown by instructor:
+
+  function reverse(word) {
+    return word.split("").reverse().join("");
+  }
+  
+  function isPalindrome(word) {
+    const reversedWord = reverse(word);
+
+    return word === reversedWord;
+  }
+
+
+  */
 
 /* 
   Add your pseudocode here
+  create a new word that is the given word in reverse 
+  if the two are the same at the end return true, else return false
 */
 
 /*
   Add written explanation of your solution here
+  if given word is a palindrome it should be the same final word after it is reversed
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +40,7 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
